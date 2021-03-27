97bcce366a656931f043e25a40f2c0590d589fb9
Guillaume Roux <guillaume2.roux@epitech.eu>
2021-03-27 19:32:08 +0100

    migrated to nullsafety

--- lib/image_picker_web_redux.dart
+++ lib/image_picker_web_redux.dart
@@ -35,18 +35,18 @@ class ImagePickerWeb {
   static const MethodChannel _methodChannel =
       const MethodChannel('image_picker_web');
 
-  static Future<html.File> _pickFile(String type) async {
+  static Future<html.File?> _pickFile(String type) async {
     final html.FileUploadInputElement input = html.FileUploadInputElement();
     input.accept = '$type/*';
     input.click();
-    html.document.body.append(input);
+    html.document.body!.append(input);
     await input.onChange.first;
-    if (input.files.isEmpty) return null;
-    return input.files[0];
+    if (input.files!.isEmpty) return null;
+    return input.files![0];
   }
 
   /// source: https://stackoverflow.com/a/59420655/9942346
-  Future<List<html.File>> _pickMultiFiles(String type) async {
+  Future<List<html.File>?> _pickMultiFiles(String type) async {
     final completer = Completer<List<html.File>>();
     final html.FileUploadInputElement input = html.FileUploadInputElement();
     input.multiple = true;
@@ -54,7 +54,7 @@ class ImagePickerWeb {
     input.click();
     // onChange doesn't work on mobile safari.
     input.addEventListener('change', (e) async {
-      final files = input.files;
+      final files = input.files!;
       Iterable<Future<html.File>> resultsFutures = files.map((file) async {
         final reader = html.FileReader();
         reader.readAsDataUrl(file);
@@ -65,7 +65,7 @@ class ImagePickerWeb {
       completer.complete(results);
     });
     // Need to append on mobile Safari.
-    html.document.body.append(input);
+    html.document.body!.append(input);
     final results = await completer.future;
     if (results.isEmpty) return null;
     return results;
@@ -74,19 +74,18 @@ class ImagePickerWeb {
   /// Picker that close after selecting 1 image. Here are the different instance
   /// of Future returned depending on [outputType] :
   ///
-  /// * [ImageType.file] return a [html.File] object of the selected image.
+  /// - [ImageType.file] return a [html.File] object of the selected image.
   ///
-  /// * [ImageType.bytes] return a [Uint8List] of the selected image.
+  /// - [ImageType.bytes] return a [Uint8List] of the selected image.
   ///
-  /// * [ImageType.widget] return an [Image.memory] using the image's bytes.
+  /// - [ImageType.widget] return an [Image.memory] using the image's bytes.
   ///
   /// ```dart
   /// html.File imgFile = await getImage(ImageType.file);
   /// Uint8List imgBytes = await getImage(ImageType.bytes);
   /// Image imgWidget = await getImage(ImageType.widget);
   /// ```
-  static Future<dynamic> getImage({@required ImageType outputType}) async {
-    assert(outputType != null);
+  static Future<dynamic> getImage({required ImageType outputType}) async {
     if (!(outputType is ImageType)) {
       throw ArgumentError(
           'outputType has to be from Type: ImageType if you call getImage()');
@@ -108,18 +107,19 @@ class ImagePickerWeb {
   /// Return an object [MediaInfo] containing image's informations.
   static Future<MediaInfo> get getImageInfo async {
     final data =
-        await _methodChannel.invokeMapMethod<String, dynamic>('pickImage');
+        await (_methodChannel.invokeMapMethod<String, dynamic>('pickImage')
+            as FutureOr<Map<String, dynamic>>);
     return MediaInfo.fromJson(data);
   }
 
   // Picker allow multi-image selection. Here are the different instance
   /// of Future returned depending on [outputType] :
   ///
-  /// * [ImageType.file] return a [html.File] list of the selected images.
+  /// - [ImageType.file] return a [html.File] list of the selected images.
   ///
-  /// * [ImageType.bytes] return a [Uint8List] list of the selected images.
+  /// - [ImageType.bytes] return a [Uint8List] list of the selected images.
   ///
-  /// * [ImageType.widget] return an [Image.memory] list using the images'
+  /// - [ImageType.widget] return an [Image.memory] list using the images'
   /// bytes.
   ///
   /// ```dart
@@ -127,8 +127,7 @@ class ImagePickerWeb {
   /// List<Uint8List> imgBytes = await getMultiImages(ImageType.bytes);
   /// List<Image> imgWidgets = await getMultiImages(ImageType.widget);
   /// ```
-  static Future<List> getMultiImages({@required ImageType outputType}) async {
-    assert(outputType != null);
+  static Future<List?> getMultiImages({required ImageType outputType}) async {
     if (!(outputType is ImageType)) {
       throw ArgumentError(
           'outputType has to be from Type: ImageType if you call getImage()');
@@ -155,22 +154,22 @@ class ImagePickerWeb {
   /// Picker that close after selecting 1 video. Here are the different instance
   /// of Future returned depending on [outputType] :
   ///
-  /// * [VideoType.file] return a [html.File] object of the selected video.
+  /// - [VideoType.file] return a [html.File] object of the selected video.
   ///
-  /// * [VideoType.bytes] return a [Uint8List] of the selected video.
+  /// - [VideoType.bytes] return a [Uint8List] of the selected video.
   ///
   /// ```dart
   /// html.File videoFile = await getVideo(VideoType.file);
   /// Uint8List videoBytes = await getVideo(VideoType.bytes);
   /// ```
-  static Future<dynamic> getVideo({@required VideoType outputType}) async {
-    assert(outputType != null);
+  static Future<dynamic> getVideo({required VideoType outputType}) async {
     switch (outputType) {
       case VideoType.file:
         return ImagePickerWeb._pickFile('video');
       case VideoType.bytes:
         final data =
-            await _methodChannel.invokeMapMethod<String, dynamic>('pickVideo');
+            await (_methodChannel.invokeMapMethod<String, dynamic>('pickVideo')
+                as FutureOr<Map<String, dynamic>>);
         final imageData = base64.decode(data['data']);
         return imageData;
       default:
@@ -183,7 +182,8 @@ class ImagePickerWeb {
   /// Return an object [MediaInfo] containing video's informations.
   static Future<MediaInfo> get getVideoInfo async {
     final data =
-        await _methodChannel.invokeMapMethod<String, dynamic>('pickVideo');
+        await (_methodChannel.invokeMapMethod<String, dynamic>('pickVideo')
+            as FutureOr<Map<String, dynamic>>);
     return MediaInfo.fromJson(data);
   }
 }
--- lib/src/web_image_picker.dart
+++ lib/src/web_image_picker.dart
@@ -2,41 +2,41 @@ import 'dart:async';
 import 'dart:html' as html;
 
 class WebImagePicker {
-  Future<Map<String, dynamic>> pickImage() async {
+  Future<Map<String, dynamic>?> pickImage() async {
     final Map<String, dynamic> data = {};
     final html.FileUploadInputElement input = html.FileUploadInputElement();
     input..accept = 'image/*';
     input.click();
-    html.document.body.append(input);
+    html.document.body!.append(input);
     await input.onChange.first;
-    if (input.files.isEmpty) return null;
+    if (input.files!.isEmpty) return null;
     final reader = html.FileReader();
-    reader.readAsDataUrl(input.files[0]);
+    reader.readAsDataUrl(input.files![0]);
     await reader.onLoad.first;
     final encoded = reader.result as String;
     final stripped =
         encoded.replaceFirst(RegExp(r'data:image/[^;]+;base64,'), '');
-    final imageName = input.files?.first?.name;
+    final imageName = input.files?.first.name;
     data.addAll({'name': imageName, 'data': stripped, 'data_scheme': encoded});
     input.remove();
     return data;
   }
 
-  Future<Map<String, dynamic>> pickVideo() async {
+  Future<Map<String, dynamic>?> pickVideo() async {
     final Map<String, dynamic> data = {};
     final html.FileUploadInputElement input = html.FileUploadInputElement();
     input..accept = 'video/*';
     input.click();
-    html.document.body.append(input);
+    html.document.body!.append(input);
     await input.onChange.first;
-    if (input.files.isEmpty) return null;
+    if (input.files!.isEmpty) return null;
     final reader = html.FileReader();
-    reader.readAsDataUrl(input.files[0]);
+    reader.readAsDataUrl(input.files![0]);
     await reader.onLoad.first;
     final encoded = reader.result as String;
     final stripped =
         encoded.replaceFirst(RegExp(r'data:video/[^;]+;base64,'), '');
-    final videoName = input.files?.first?.name;
+    final videoName = input.files?.first.name;
     data.addAll({'name': videoName, 'data': stripped, 'data_scheme': encoded});
     input.remove();
     return data;
